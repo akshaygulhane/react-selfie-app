bfd7b06a043059be97bc86d36c730a98f7053427
Akshay Gulhane <akshay.gulhane@outlook.com>
2019-01-25 10:59:10 +0530

    initial code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, NavLink, HashRouter } from 'react-router-dom';
+import Login from './components/Login';
+import Services from './components/Services';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <HashRouter>
+      <div className="container">
+        <h1 className="text-center">React Selfie App</h1>
+        <hr />
+        <div>
+          <ul className="header">
+            <li><NavLink to="/">Services</NavLink></li>
+            <li><NavLink to="/eventbrite">Eventbrite</NavLink></li>
+            <li><NavLink to="/facebook">Facebook</NavLink></li>
+          </ul>
+          <div className="content">
+            <Route exact path="/" component={Services}/>
+            <Route path="/eventbrite" component={Login}/>
+            <Route path="/facebook" component={Login}/>
+          </div>
+          </div>
+        </div>
+      </HashRouter>
     );
   }
 }
---
+++ src/components/Services.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Services extends React.Component {
+
+
+    render() {
+        return (
+            <div>
+                <h1>Services</h1>
+            </div>
+        )
+    }
+}
+
+export default Services;
